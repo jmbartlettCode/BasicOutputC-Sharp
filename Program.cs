effaaae07ff843815b6b2b7bae1666b4aa9dc5d2
Joshua Bartlett <73142613+jmbartlettCode@users.noreply.github.com>
2020-11-13 01:49:53 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿/* 
+Josh Bartlett
+23June2019
+Assignment 1.2
+Display a statement that defines program comments
+*/
+
+using System;
+
+namespace Assignment_1._2___Josh_Bartlett
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Outputs contents to console
+            Console.WriteLine("Program Comments are " +
+                "nonexecuting statements that document " +
+                "a program and programmers use these to " +
+                "leave notes for themselves and for others " +
+                "who might read their programs.");
+
+            Console.Write("Press any key to continue...");
+
+            // Keeps console open until a key is pressed
+            Console.ReadKey(true);
+        }
+    }
+}
